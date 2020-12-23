8466bd83bb16755a8a3626a2f09defc87f1b1271
Memduh Gümen <memduhgumen@gmail.com>
2020-12-23 19:33:18 +0300

    .
    .

---
+++ AYSAdalet/Migrations/202012231632161_BilgiIslemDonanim_Tablolari_Eklendi1.cs
@@ -0,0 +1,16 @@
+﻿namespace AYSAdalet.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class BilgiIslemDonanim_Tablolari_Eklendi1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
