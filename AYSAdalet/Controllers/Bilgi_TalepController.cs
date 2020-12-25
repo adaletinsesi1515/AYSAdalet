e41c9d451e9316be4787ecc04d05f99f7d96fa78
Memduh Gümen <memduhgumen@gmail.com>
2020-12-26 00:38:40 +0300

    Home tarafında güncelleme yapıldı, Bilgi İşlem için talep formu hazırlandı
    Home tarafında güncelleme yapıldı, Bilgi İşlem için talep formu hazırlandı

---
+++ AYSAdalet/Controllers/Bilgi_TalepController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AYSAdalet.Controllers
+{
+    public class Bilgi_TalepController : Controller
+    {
+        // GET: Bilgi_Talep
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
