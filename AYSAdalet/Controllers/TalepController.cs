65774f22b5ea9a3d91a29da2c6cb549e655559bc
Efe Özgür <efecuneytozgur@gmail.com>
2021-01-19 02:47:01 +0300

    İdari İşler Talep

--- AYSAdalet/Controllers/TalepController.cs
+++ AYSAdalet/Controllers/TalepController.cs
@@ -3,15 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AYSAdalet.Models.DataContext;
+using AYSAdalet.Models.Modeller;
+using AYSAdalet.ViewModels;
 
 namespace AYSAdalet.Controllers
 {
     public class TalepController : Controller
     {
-        // GET: Talep
+        AdliyeDBContext db = new AdliyeDBContext();
+        PersonelTalepVM pt = new PersonelTalepVM();
+
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Index(string sicil)
+        {
+            var sicilbul = db.Personel.Where(x => x.PersonelSicil == sicil).FirstOrDefault();
+
+            if (sicilbul != null)
+            {
+                var gorevyeribul = db.PersonelGorevYerleri.Where(x => x.PersonelID == sicilbul.PersonelID).ToList();
+                pt.Personel = sicilbul;
+                pt.GorevYerleri = gorevyeribul;
+                pt.Birimler = db.Birimler.ToList();
+                return View(pt);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult KaydaYolla()
+        {
+            ViewBag.mesaj = Request.Form["mesaj"];   
+            
+            return View();
+        }
+
     }
 }
---
+++ AYSAdalet/ViewModels/PersonelTalepVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Web;
+using AYSAdalet.Models.Modeller;
+
+namespace AYSAdalet.ViewModels
+{
+    public class PersonelTalepVM
+    {
+        public List<Birimler> Birimler { get; set; }
+        public List<PersonelGorevYerleri> GorevYerleri { get; set; }
+        public Personel Personel { get; set; }
+    }
+}
