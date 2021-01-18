442b875ddb3bdca8acc9dbc49db9cf782947d606
Memduh Gümen <memduhgumen@gmail.com>
2021-01-18 21:52:17 +0300

    son çalışma önemsiz
    son çalışma önemsiz

--- AYSAdalet/Controllers/Bilgi_TalepController.cs
+++ AYSAdalet/Controllers/Bilgi_TalepController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using AYSAdalet.Models.DataContext;
 using AYSAdalet.Models.Modeller;
 
@@ -16,32 +17,28 @@ namespace AYSAdalet.Controllers
 
         public ActionResult Index(string t)
         {
-            //var Model = db.Personel.FirstOrDefault(x => x.PersonelSicil.Sta || t != null && x.Durum == true).ToList();
-            var Model = db.Personel.FirstOrDefault(x => x.PersonelSicil == t  && x.Durum == true);
-            ////List<Personel> listemp = db.Personel.Where(x => x.Durum== true).ToList();
-            
+            var Model = db.Personel.FirstOrDefault(x => x.PersonelSicil == t && x.Durum == true);
+
+            //var Model = db.Personel.Where(x => x.PersonelSicil == t && x.Durum == true).ToList();
             return View(Model);
-            
-            
-            //return View();
 
         }
-        
+
         [HttpPost]
-        public ActionResult MesajKayit(int PersonelId,string txtMsg)
+        public ActionResult MesajKayit(int PersonelId, string txtMsg)
         {
 
             var UserModel = db.Personel.FirstOrDefault(x => x.PersonelID == PersonelId);
             BilgiTalepler _BlgTlp = new BilgiTalepler
             {
-                PersonelId=UserModel.PersonelID,
+                PersonelId = UserModel.PersonelID,
                 TalepMesaji = txtMsg,
                 BildirimTarihi = DateTime.Now,
                 Durum = true
             };
             db.BilgiTalepler.Add(_BlgTlp);
             db.SaveChanges();
-            TempData["mesaj"]= "Mesajınız sisteme iletilmiştir....";
+            TempData["mesaj"] = "Mesajınız sisteme iletilmiştir....";
             return Redirect("Index");
         }
     }
