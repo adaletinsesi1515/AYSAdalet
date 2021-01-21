878701f0b1a28d75de95953c97fed1c039c6cf55
Efe Özgür <efecuneytozgur@gmail.com>
2021-01-21 20:19:15 +0300

    Nöbet

---
+++ AYSAdalet/Content/NobetAssets/js/TarihSec.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    let now = new Date();
+
+    let day = ("0" + now.getDate()).slice(-2);
+    let month = ("0" + (now.getMonth() + 1)).slice(-2);
+
+    let today = (day) + "-" + (month) + "-" + now.getFullYear();
+
+
+    $('#datePicker').val(today);
+
+    $('#datebtn').click(function () {
+
+        testClicked();
+
+    });
+});
+function testClicked() {
+    $('.getDate').html($('#datePicker').val());
+}
---
+++ AYSAdalet/Controllers/NobetController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AYSAdalet.Models.DataContext;
+using AYSAdalet.ViewModels;
+
+namespace AYSAdalet.Controllers
+{
+    public class NobetController : Controller
+    {
+        AdliyeDBContext db = new AdliyeDBContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult NobeTanim()
+        {
+            NobetBilgiListVm vm = new NobetBilgiListVm
+            {
+                Personels = db.Personel.ToList(),
+            };
+            return View(vm);
+        }
+    }
+}
